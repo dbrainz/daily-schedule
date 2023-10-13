1db45a8f7275cea4ac3d0ec2d4990446e2371e22
Darren Brain <darren.brain@gmail.com>
2023-10-13 01:36:42 -0400

    Update block colors based on current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+function updateBlockColors() {
+  var currentHour=dayjs().hour();
+  for (blockNum=9; blockNum<18; blockNum++) {
+    if (blockNum<currentHour){
+      $("#hour-" + blockNum).addClass("past");
+      $("#hour-" + blockNum).removeClass("present future");
+    } else if (blockNum==currentHour){
+      $("#hour-" + blockNum).addClass("present");
+      $("#hour-" + blockNum).removeClass("past future");
+    } else {
+      $("#hour-" + blockNum).addClass("future");
+      $("#hour-" + blockNum).removeClass("past present");
+    }
+  }
+}
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +36,11 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  console.log(dayjs().hour());
+  console.log(dayjs().format('MM/DD/YYYY'))
+  updateBlockColors();
+
+
+
 });
+
